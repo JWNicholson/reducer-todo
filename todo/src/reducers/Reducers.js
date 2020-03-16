18846dca3e6d5b3ff92991a832151fdd5e3adfa4
JW Nicholson <john.nicholson812@gmail.com>
2020-03-16 16:02:49 -0400

    setting up state and ADD/DElETE

--- todo/src/reducers/Reducers.js
+++ todo/src/reducers/Reducers.js
@@ -5,6 +5,20 @@ export const initialState = {
         id: 3892987589
 }
 
-export const titleReducer(){
-    return state;
+export const todoReducer = (state, action) => {
+    switch(action.type) {
+   
+    case "ADD_TODO" :
+    return [...state, action.payload];
+    case "DELETE_TODO":
+    return state.filter(item => {
+      //this will filter and keep whatever is false, and whatever is true it will delete
+      return !item.completed
+    }) 
+     
+    
+    default:
+return state
+}
+
 }
